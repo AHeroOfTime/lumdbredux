4594fa1572e56e44b770a5a311c9596f8fe96aeb
Aaron <aaroner@yahoo.com>
2019-08-06 16:00:02 -0500

    10/21

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Toggle = ({ messageVisibility }) => (
+const Toggle = ({ messageVisibility, dispatch }) => (
   <div>
     {messageVisibility && <p>This is toggleable text!</p>}
-    <button>Toggle Me</button>
+    <button
+      onClick={() =>
+        dispatch({
+          type: 'TOGGLE_MESSAGE',
+        })
+      }
+    >
+      Toggle Me
+    </button>
   </div>
 );
 
