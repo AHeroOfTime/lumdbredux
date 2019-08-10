e1f1d5bc44f75fcfc4b5a4f2e6551a7e43b6acfb
Aaron <aaroner@yahoo.com>
2019-08-09 19:47:23 -0500

    14/21

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 
 import logo from './logo.svg';
 import './App.css';
@@ -14,7 +15,13 @@ import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
 import Toggle from './Toggle';
 
-const store = createStore(rootReducer, {}, composeWithDevTools());
+const middleware = [logger];
+
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware(...middleware)),
+);
 
 const App = () => (
   <Provider store={store}>
@@ -25,7 +32,7 @@ const App = () => (
             <img src={logo} className="App-logo" alt="logo" />
           </Link>
         </header>
-        <Toggle />>
+        <Toggle />
         <Switch>
           <Route exact path="/" component={MoviesList} />
           <Route path="/:id" component={MovieDetail} />
